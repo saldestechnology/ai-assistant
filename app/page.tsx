298bacfdb5e130f0742a11c710fad1b89d500949
Johan Saldes <johan@saldestechnology.com>
2024-03-10 15:45:12 +0100

    basic layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import React, { useEffect, useRef, useState } from "react";
+import { FaMicrophone, FaMicrophoneSlash } from "react-icons/fa";
+import { RxPaperPlane } from "react-icons/rx";
 
 export default function Home() {
   const stream = useRef<MediaStream | null>(null);
@@ -37,6 +39,10 @@ export default function Home() {
     initAudioCapturing();
   }, [model]);
 
+  const handleSelectModel = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setModel(e.target.value);
+  };
+
   const handleRecording = async () => {
     if (!mediaRecorder.current) {
       return;
@@ -52,20 +58,11 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <section className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center h-full">
-        <h1 className="text-4xl font-bold text-center mb-4">
-          AI Assistant: Jeffrey
-        </h1>
-        <p className="text-lg">
-          Press the record button and ask a question to Jeffrey.
-        </p>
-        <p className="text-lg">Please pick a completion model.</p>
+    <div className="flex flex-col h-full">
+      <section className="flex">
         <select
-          className="outline-none border border-white px-4 py-2 rounded-md mt-4 bg-black text-white"
-          onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-            setModel(e.target.value);
-          }}
+          onChange={handleSelectModel}
+          className="bg-black border border-white text-white"
         >
           <option value="gpt-3.5-turbo">GPT-3.5 Turbo</option>
           <option value="gpt-4">GPT-4</option>
@@ -74,15 +71,30 @@ export default function Home() {
           <option value="open-mixtral-8x7b">open-mixtral-8x7b</option>
           <option value="mistral-small-latest">mistral-small-latest</option>
         </select>
-        <button
-          className="outline-none border border-white px-4 py-2 rounded-md mt-4"
-          onClick={handleRecording}
-        >
-          {isRecording ? "Recording..." : "Record"}
-        </button>
       </section>
-      <section className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center h-full">
-        {isLoading && <p className="m-0 p-0">Loading...</p>}
+      <section className="flex w-full h-full">
+        <p>Chat area</p>
+      </section>
+      <section className="flex justify-start">
+        <button
+          onClick={handleRecording}
+          className={`${
+            isRecording ? "bg-red-500" : "bg-blue-500"
+          } text-white px-4 py-2`}
+        >
+          {isRecording ? <FaMicrophoneSlash /> : <FaMicrophone />}
+        </button>
+        <input
+          type="text"
+          placeholder="Enter your message"
+          className="w-full bg-black border border-white leading-8 px-2"
+        />
+        <button
+          onClick={handleRecording}
+          className="bg-blue-500 text-white px-4 py-2"
+        >
+          <RxPaperPlane />
+        </button>
       </section>
     </div>
   );
